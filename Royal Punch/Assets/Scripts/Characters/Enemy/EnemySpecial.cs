ce89357b63f1716304ee1fecf0d66d9c3c391fab
dimenatar <dimenatar25@gmail.com>
2022-06-02 17:20:49 +0300

    Stream fix

--- Royal Punch/Assets/Scripts/Characters/Enemy/EnemySpecial.cs
+++ Royal Punch/Assets/Scripts/Characters/Enemy/EnemySpecial.cs
@@ -95,8 +95,8 @@ public class EnemySpecial : MonoBehaviour
         if (!_enemyFight.IsInFight && !_isInSpecialAttack && !_isTired)
         {
             // pick random attack through all of types
-            //SpecialAttacks attack = SpecialAttacks.Dragging;
-            SpecialAttacks attack = (SpecialAttacks) UnityEngine.Random.Range(0, Enum.GetNames(typeof(SpecialAttacks)).Length);
+            SpecialAttacks attack = SpecialAttacks.Stream;
+            //SpecialAttacks attack = (SpecialAttacks) UnityEngine.Random.Range(0, Enum.GetNames(typeof(SpecialAttacks)).Length);
             StartAttack(attack);
         }
         else
